4df73a311ec32d47a890b0350110b078eb120d8f
jungho1123 <snsdl1123@naver.com>
2025-07-31 14:42:04 +0900

    refactor(run.py):주석참고

--- pet_project_backend/run.py
+++ pet_project_backend/run.py
@@ -3,8 +3,10 @@
 from app import create_app
 import os
 
-env = os.getenv('FLASK_ENV', 'development')
-app = create_app(env)
+# create_app 함수가 내부에서 'FLASK_ENV'를 직접 읽으므로
+# 이 부분은 더 이상 필요하지 않습니다.
+# env = os.getenv('FLASK_ENV', 'development') 
+app = create_app()
 
 if __name__ == '__main__':
     app.run(
