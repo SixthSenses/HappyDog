99bb3e5769cad76ddf58a17c0e709bf422995ded
kingy0ujin <godjin11@naver.com>
2025-08-08 13:32:06 +0900

    chore: .gitignore 업데이트 및 디버그 모드 활성화

--- pet_project_backend/run.py
+++ pet_project_backend/run.py
@@ -8,5 +8,5 @@ app = create_app()
 if __name__ == '__main__':
     host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
     port = int(os.getenv('FLASK_RUN_PORT', 5000))
-    debug = app.config.get('DEBUG', False)
+    debug = app.config.get('DEBUG', True)
     app.run(host=host, port=port, debug=debug)
