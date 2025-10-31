a5cd45fe933171ba93c2946ee1216dbe76347c7c
jungho1123 <snsdl1123@naver.com>
2025-10-31 17:06:49 +0900

    petinfo에 verified 추가

--- pet_project_backend/app/models/post.py
+++ pet_project_backend/app/models/post.py
@@ -19,6 +19,7 @@ class PetInfo:
     breed: str
     birthdate: datetime
     profile_image_url: Optional[str] = None  # Pet 프로필 이미지 추가
+    is_verified: bool = False  # 생체 인증 여부 (비문/안구 분석)
 
 @dataclass
 class Post:
